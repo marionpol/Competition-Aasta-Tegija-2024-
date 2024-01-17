208d750756ad70b9a7e3a93edbd7c965244a7212
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-01-17 19:45:30 +0000

    ABOUT

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+function changeIcon(button, paragraphId) {
+    // Reset all arrows to the left position
+    var allButtons = document.querySelectorAll('.arrow-btn');
+    allButtons.forEach(function (btn) {
+        var icon = btn.querySelector('i');
+        icon.classList.remove('fa-arrow-down');
+        icon.classList.add('fa-arrow-left');
+    });
+
+    // Toggle the arrow for the clicked button
+    var icon = button.querySelector('i');
+    icon.classList.toggle('fa-arrow-left');
+    icon.classList.toggle('fa-arrow-down');
+
+    // Toggle the visibility of paragraphs based on the clicked button
+    var paragraphs = document.querySelectorAll('.about-content');
+    paragraphs.forEach(function (paragraph) {
+        paragraph.classList.remove('active');
+    });
+
+    var activeParagraphId = button.getAttribute('data-paragraph');
+    var activeParagraph = document.getElementById(activeParagraphId);
+    activeParagraph.classList.add('active');
+}
 document.addEventListener("DOMContentLoaded", function () {
     const logosContainer = document.getElementById("logod-container");
     const originalLogos = logosContainer.innerHTML; 
