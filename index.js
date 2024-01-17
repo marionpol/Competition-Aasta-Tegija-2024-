1aefc688e5b90615d1ed314bff4a8a59e206c2f4
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-01-17 09:19:29 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    const logoContainer = document.querySelector(".sponsor");
+    let position = 0;
+  
+    function moveLogos() {
+      position -= 5; // Adjust the speed as needed
+      logoContainer.style.transform = `translateX(${position}px)`;
+  
+      // Repeat the animation
+      if (position <= -100 * 7) {
+        position = 0;
+        logoContainer.style.transition = "none";
+        logoContainer.style.transform = `translateX(${position}px)`;
+        setTimeout(() => {
+          logoContainer.style.transition = "transform 0.5s ease-in-out";
+        }, 0);
+      }
+  
+      requestAnimationFrame(moveLogos);
+    }
+  
+    moveLogos();
+  });
