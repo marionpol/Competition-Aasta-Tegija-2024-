2f6f33ddd0cbfd8f1dd78faee72859b1357147f7
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-01-17 16:08:57 +0000

    sponsorid

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var logosContainer = document.querySelector('.logos-container');
+    const logosContainer = document.getElementById("logod-container");
+    const originalLogos = logosContainer.innerHTML; 
 
-    function startAnimation() {
-        // Move the logos to the left
-        logosContainer.style.transform = 'translateX(-100%)';
+    logosContainer.style.animation = "scroll 60s linear infinite";
+
+    for (let i = 0; i < 4; i++) {
+        logosContainer.innerHTML += originalLogos;
     }
-
-    startAnimation();
-
-    logosContainer.addEventListener('transitionend', function () {
-        // Move the logos instantly to the starting position without noticeable jump
-        logosContainer.style.transition = 'none';
-        logosContainer.style.transform = 'translateX(0)';
-        void logosContainer.offsetWidth; // Trigger reflow
-        setTimeout(function () {
-            // Restart the animation
-            logosContainer.style.transition = 'transform 2s linear'; // Adjust the duration as needed
-            startAnimation();
-        }, 0);
-    });
 });
