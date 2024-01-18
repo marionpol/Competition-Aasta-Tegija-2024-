b43d7c68ac67b46ce538f02f8f5327670f9d1f67
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-01-18 06:38:20 +0000

    js

--- index.js
+++ index.js
@@ -33,14 +33,18 @@ function changeIcon(button, paragraphId) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const logosContainer = document.getElementById("logod-container");
-    const originalLogos = logosContainer.innerHTML; 
+  const logosContainer = document.getElementById("logod-container");
+  const originalLogos = logosContainer.innerHTML; 
 
-    logosContainer.style.animation = "scroll 60s linear infinite";
+  logosContainer.style.backgroundColor = "white";
 
-    for (let i = 0; i < 4; i++) {
-        logosContainer.innerHTML += originalLogos;
-    }
+  logosContainer.style.minHeight = "100vh";
+
+  logosContainer.style.animation = "scroll 60s linear infinite";
+
+  for (let i = 0; i < 4; i++) {
+      logosContainer.innerHTML += originalLogos;
+  }
 });
 
 // Nav bar background fade in and out
